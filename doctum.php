3938c56ba19e9ba049d145b1c89a8b6d5a51d9f1
Morten Harders <mmh@harders-it.dk>
2023-07-06 22:10:50 +0200

    Added OpenSearch support

--- doctum.php
+++ doctum.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 use Doctum\RemoteRepository\GitHubRemoteRepository;
 
 return new Doctum\Doctum(__DIR__ . '/src', [
-    'title'                => 'ReMix',
+    'title'                => 'ReMix API Documentation',
     'build_dir'            => __DIR__ . '/docs',
     'cache_dir'            => __DIR__ . '/.doctum.cache',
     'default_opened_level' => 2,
     'remote_repository'    => new GitHubRemoteRepository('ExeQue/remix', __DIR__),
+    'base_url'             => 'https://exeque.github.io/remix/',
 ]);
